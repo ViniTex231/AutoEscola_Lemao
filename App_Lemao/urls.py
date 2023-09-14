105432c3f5f32f1a3277e84cdcdcf39e87e76a40
ViniTex231 <viniteixeira2828@gmail.com>
2023-09-14 16:54:13 -0300

    create a consult pages

--- App_Lemao/urls.py
+++ App_Lemao/urls.py
@@ -3,11 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.abre_index, name='abre_index'),
-    path('cad_carros', views.cad_carros, name='cad_carros'),
-    path('cad_drivers', views.cad_drivers, name='cad_drivers'),
-    path('salvar_driver_novo', views.salvar_driver_novo, name='salvar_driver_novo'),
-    path('salvar_carro_novo', views.salvar_carro_novo, name='salvar_carro_novo'),
-    path('cons_carros', views.cons_carros, name='cons_carros'),
+    path('carros/', views.carros, name='carros'),
+    path('drivers/', views.drivers, name='drivers'),
+    path('cad_carros/', views.cad_carros, name='cad_carros'),
+    path('cad_drivers/', views.cad_drivers, name='cad_drivers'),
+    path('salvar_driver_novo/', views.salvar_driver_novo, name='salvar_driver_novo'),
+    path('salvar_carro_novo/', views.salvar_carro_novo, name='salvar_carro_novo'),
+    path('cons_carros/', views.cons_carros, name='cons_carros'),
+    path('cons_drivers/', views.cons_drivers, name='cons_drivers'),
     
 
 ]
--- App_Lemao/views.py
+++ App_Lemao/views.py
@@ -8,11 +8,17 @@ from .models import Carros, Driver
 def abre_index(request):
     return render(request, 'index.html')
 
-def cad_carros(request):
+def carros(request):
     return render(request, 'carros.html')
 
+def drivers(request):
+    return render(request, 'drivers.html')
+
+def cad_carros(request):
+    return render(request, 'cad_carros.html')
+
 def cad_drivers(request):
-    return render(request, 'alunos.html')
+    return render(request, 'cad_alunos.html')
 
 def salvar_driver_novo(request):
     if (request.method == 'POST'):
@@ -26,7 +32,7 @@ def salvar_driver_novo(request):
         )
         grava_driver.save()
         messages.info(request, 'Motorista ' + nome + ' cadastrado com sucesso!')
-        return render(request, 'alunos.html')
+        return render(request, 'cad_alunos.html')
     
 def salvar_carro_novo(request):
     if (request.method == 'POST'):
@@ -44,9 +50,75 @@ def salvar_carro_novo(request):
         )
         grava_carro.save()
         messages.info(request, 'Veículo ' + nome + ' cadastrado com sucesso!')
-        return render(request, 'carros.html')
+        return render(request, 'cad_carros.html')
     
 def cons_carros(request):
     dado_pesquisa_nome = request.POST.get('nomeCarro')
     dado_pesquisa_placa = request.POST.get('placaCarro')
     dado_pesquisa_dono = request.POST.get('donoCarro')
+
+    if dado_pesquisa_nome or dado_pesquisa_placa or dado_pesquisa_dono:
+        page = request.GET.get('page')
+
+        if page:
+            dado_pesquisa = request.GET.get(dado_pesquisa)
+            carros_lista = Carros.objects.filter(nome__icontains=dado_pesquisa_nome)
+            paginas = Paginator(carros_lista, 10000)
+            carros = paginas.get_page(page)
+
+            return render(request, 'cons_carros.html', {'dados_carros' : carros, 'dado_pesquisa' : dado_pesquisa})
+        
+        if dado_pesquisa_nome != None and dado_pesquisa_nome != '':
+            carros_lista = Carros.objects.filter(nome__icontains=dado_pesquisa_nome)
+            paginas = Paginator(carros_lista, 10000)
+            page = request.GET.get('page')
+            carros = paginas.get_page(page)
+
+            return render(request, 'cons_carros.html', {'dados_carros' : carros, 'dado_pesquisa' : dado_pesquisa_nome})
+        
+        elif dado_pesquisa_placa != None and dado_pesquisa_placa != '':
+            carros = Carros.objects.filter(cnh__icontains=dado_pesquisa_placa)
+            return render(request, 'cons_carros.html', {'dados_carros' : carros})
+        
+        elif dado_pesquisa_dono != None and dado_pesquisa_dono != '':
+            carros = Carros.objects.filter(dono__icontains=dado_pesquisa_dono)
+            return render(request, 'cons_carros.html', {'dados_carros' : carros})
+        
+        else:
+            return render(request, 'cons_carros.html')
+    else:
+        carros = Carros.objects.all()
+        return render(request, 'cons_carros.html', {'dados_carros' : carros})
+    
+def cons_drivers(request):
+    dado_pesquisa_nome = request.POST.get('nomeDriver')
+    dado_pesquisa_cnh = request.POST.get('cnhDriver')
+
+    if dado_pesquisa_nome or dado_pesquisa_cnh:
+        page = request.GET.get('page')
+
+        if page:
+            dado_pesquisa = request.GET.get(dado_pesquisa)
+            drivers_lista = Driver.objects.filter(nome__icontains=dado_pesquisa_nome)
+            paginas = Paginator(drivers_lista, 10000)
+            drivers = paginas.get_page(page)
+
+            return render(request, 'cons_drivers.html', {'dados_drivers' : drivers, 'dado_pesquisa' : dado_pesquisa})
+        
+        if dado_pesquisa_nome != None and dado_pesquisa_nome != '':
+            drivers_lista = Driver.objects.filter(nome__icontains=dado_pesquisa_nome)
+            paginas = Paginator(drivers_lista, 10000)
+            page = request.GET.get('page')
+            drivers = paginas.get_page(page)
+
+            return render(request, 'cons_drivers.html', {'dados_drivers' : drivers, 'dado_pesquisa' : dado_pesquisa_nome})
+        
+        elif dado_pesquisa_cnh != None and dado_pesquisa_cnh != '':
+            drivers = Driver.objects.filter(cnh__icontains=dado_pesquisa_cnh)
+            return render(request, 'cons_drivers.html', {'dados_drivers' : drivers})
+        
+        else:
+            return render(request, 'cons_drivers.html')
+    else:
+        drivers = Driver.objects.all()
+        return render(request, 'cons_drivers.html', {'dados_drivers' : drivers})
